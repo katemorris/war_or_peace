0778158e40e5d1030d1e8b837e4ce308a4532ee5
Kate Tester <katemorris@gmail.com>
2020-08-17 20:54:28 -0600

    Adds initial deck files

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,13 @@
 class Card
   attr_reader :rank, :suit, :value
+  attr_accessor :cards
 
   def initialize(suit, value, rank)
     @rank = rank
     @suit = suit
     @value = value
+    @cards = []
+    @cards << self
   end
+
 end
---
+++ lib/deck.rb
@@ -0,0 +1,30 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    return @cards[index].rank
+  end
+
+  def high_ranking_cards
+    # this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+    @cards.select do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+    # this method will return the percentage of cards that are high ranking
+  end
+
+  def remove_card
+    # this method will remove the top card from the deck
+  end
+
+  def add_card
+    # this method will add one card to the bottom (end) of the deck
+  end
+end
