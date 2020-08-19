1e5a75e6d2858cb95c67ceaa4e804e156e93e2f7
Kate Tester <katemorris@gmail.com>
2020-08-19 14:09:57 -0600

    Adds cards and a card deck

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,3 +1,7 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
     #
     # Create 52 Cards (A standard deck)
     # Put those card into two Decks (some randomness would be nice here!)
@@ -63,3 +67,11 @@ card49 = Card.new(:diamond, 'Jack', 11)
 card50 = Card.new(:diamond, 'Queen', 12)
 card51 = Card.new(:diamond, 'King', 13)
 card52 = Card.new(:diamond, 'Ace', 14)
+
+card_deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
+
+
+# Put those card into two Decks (some randomness would be nice here!)
+
+deck1 =
+deck2 =
