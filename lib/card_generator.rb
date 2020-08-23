280d81b5638bf307e136ca51ef319aa4df680e6c
Kate Tester <katemorris@gmail.com>
2020-08-23 12:33:24 -0600

    Removes extraneous var_name logic from card generator.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -4,14 +4,10 @@ class CardGenerator
   attr_reader :cards
 
   def initialize(filename)
-    i = 1
     card_data = File.new(filename)
-    @cards = card_data.readlines("\n", 52).map do |line|
+    @cards = card_data.readlines("\n").map do |line|
       single_card = line.split(",")
-      # require"pry"; binding.pry
-      var_name = ("card" + i.to_s).to_sym
-      i += 1
-      var_name = Card.new(single_card[1].downcase.to_sym,single_card[0],single_card[2].strip.to_i)
+      Card.new(single_card[1].downcase.to_sym,single_card[0],single_card[2].strip.to_i)
     end
     card_data.close
   end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -7,7 +7,7 @@ class CardGeneratorTest < MiniTest::Test
   def test_run
     filename = "./lib/cards.txt"
     cards = CardGenerator.new(filename).cards
-    p cards[0]
+
     assert_equal 52, cards.count
     assert_equal :heart, cards[0].suit
     assert_equal 3, cards[1].rank
