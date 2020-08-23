4b1ee8de3944fca0d561d13e47f645224ea7c14c
Kate Tester <katemorris@gmail.com>
2020-08-23 12:13:57 -0600

    Adds working code to card generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -4,10 +4,14 @@ class CardGenerator
   attr_reader :cards
 
   def initialize(filename)
-    card_data = open(filename)
-    @cards = File.readlines(filename)
-    @cards.map do |line|
-      Card.new(line[0],":"+line.downcase[1],line[2])
+    i = 1
+    card_data = File.new(filename)
+    @cards = card_data.readlines("\n", 52).map do |line|
+      single_card = line.split(",")
+      # require"pry"; binding.pry
+      var_name = ("card" + i.to_s).to_sym
+      i += 1
+      var_name = Card.new(single_card[1].downcase.to_sym,single_card[0],single_card[2].strip.to_i)
     end
     card_data.close
   end
